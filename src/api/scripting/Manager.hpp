b91ee91ee670cf4f9ab65ad8928973a4dfbad006
Amyr Ahmady <hhm6@yahoo.com>
2022-12-10 11:55:00 +0330

    remove unneeded code and make required changes in manager

--- src/api/scripting/Manager.hpp
+++ src/api/scripting/Manager.hpp
@@ -2,6 +2,7 @@
 #include "../../utils/Singleton.hpp"
 #include "sdk.hpp"
 #include "component.hpp"
+#include <nlohmann/json.hpp>
 #include "Server/Components/Pickups/pickups.hpp"
 #include "Server/Components/Objects/objects.hpp"
 #include "Server/Components/TextLabels/textlabels.hpp"
@@ -9,35 +10,29 @@
 #include "Server/Components/Checkpoints/checkpoints.hpp"
 #include "Server/Components/Actors/actors.hpp"
 
-#define UNDEFINED_FAILED_RETURN "{\"ret_value\": null}"_json
+#define STRINGIFY2(X) #X
+#define STRINGIFY(X) STRINGIFY2(X)
+
+#define UNDEFINED_FAILED_RETURN "{\"ret_value\":undefined}"_json
+#define RETURN_VALUE(x) valueSetJsonReturnValue(x)
+#define NO_DATA_SUCCESS_RETURN "{\"ret_value\":true}"_json
 
 class OmpManager : public Singleton<OmpManager>
 {
 public:
 	/// Store open.mp components
-	void Init(ICore* c, IComponentList* clist)
-	{
-		core = c;
-		componentList = clist;
-		players = &core->getPlayers();
+	void Init(ICore* c, IComponentList* clist);
 
-		actors = GetComponent<IActorsComponent>();
-		objects = GetComponent<IObjectsComponent>();
-		pickups = GetComponent<IPickupsComponent>();
-		textlabels = GetComponent<ITextLabelsComponent>();
-		vehicles = GetComponent<IVehiclesComponent>();
-	}
+	// Add event handlers to components' event dispatchers
+	void AddEvents();
+
+	// Remove event handlers from components' event dispatchers
+	void RemoveEvents();
 
 	/// Get open.mp core instance
 	ICore* GetCore()
 	{
 		return core;
-	}
-
-	/// Get player pool
-	IPlayerPool* GetPlayers()
-	{
-		return players;
 	}
 
 	/// Get an open.mp component
@@ -47,168 +42,41 @@ public:
 		return componentList->queryComponent<ComponentType>();
 	}
 
-	/// Get pickups component
-	IPickupsComponent* GetPickups()
-	{
-		return pickups;
-	}
-
-	/// Get objects component
-	IObjectsComponent* GetObjects()
-	{
-		return objects;
-	}
-
-	/// Get vehicles component
-	IVehiclesComponent* GetVehicles()
-	{
-		return vehicles;
-	}
-
-	/// Get textlabels component
-	ITextLabelsComponent* GetTextLabels()
-	{
-		return textlabels;
-	}
-
-	/// Get actors component
-	IActorsComponent* GetActors()
-	{
-		return actors;
-	}
-
-private:
-	ICore* core = nullptr;
-	IComponentList* componentList = nullptr;
 	IPlayerPool* players = nullptr;
 	IPickupsComponent* pickups = nullptr;
 	IObjectsComponent* objects = nullptr;
 	ITextLabelsComponent* textlabels = nullptr;
 	IVehiclesComponent* vehicles = nullptr;
 	IActorsComponent* actors = nullptr;
+
+private:
+	ICore* core = nullptr;
+	IComponentList* componentList = nullptr;
 };
 
 /// Get player from players pool
-inline IPlayer* GetPlayer(int playerid)
-{
-	IPlayerPool* pool = OmpManager::Get()->GetPlayers();
-	if (pool == nullptr)
-	{
-		return nullptr;
-	}
-
-	return pool->get(playerid);
-}
-
-/// Get player from players pool
 template<typename PlayerDataType>
-inline PlayerDataType* GetPlayerData(int playerid)
+inline PlayerDataType* GetPlayerData(IPlayer* player)
 {
-	IPlayer* player = GetPlayer(playerid);
 	if (player == nullptr)
 	{
 		return nullptr;
 	}
-
 	return queryExtension<PlayerDataType>(*player);
 }
 
-/// Get player entity using provided player id and entity id
-template<typename PlayerEntityPoolType, typename EntityType>
-inline EntityType* GetPlayerEntity(int playerid, int entity_id)
+template<typename T>
+inline nlohmann::json valueSetJsonReturnValue(T value)
 {
-	PlayerEntityPoolType* pool = GetPlayerData<PlayerEntityPoolType>(playerid);
-	if (pool == nullptr)
-	{
-		return nullptr;
-	}
-
-	return pool->get(entity_id);
-}
-
-/// Get entity using provided entity id
-template<typename EntityType, typename EntityPoolType>
-inline EntityType* GetEntity(int entity_id)
-{
-	EntityPoolType* pool = OmpManager::Get()->GetComponent<EntityPoolType>();
-	if (pool == nullptr)
-	{
-		return nullptr;
-	}
-
-	return pool->get(entity_id);
-}
-
-/// Just a dummy function and it returns nullptr, overloads will do the job
-template<typename EntityType>
-inline EntityType* GetEntity(int entity_id)
-{
-	return nullptr;
-}
-
-template<>
-inline IPickup* GetEntity<IPickup>(int entity_id)
-{
-	IPickupsComponent* pickups = OmpManager::Get()->GetPickups();
-	if (pickups == nullptr)
-	{
-		return nullptr;
-	}
-
-	return pickups->get(entity_id);
-}
-
-template<>
-inline IVehicle* GetEntity<IVehicle>(int entity_id)
-{
-	IVehiclesComponent* vehicles = OmpManager::Get()->GetVehicles();
-	if (vehicles == nullptr)
-	{
-		return nullptr;
-	}
-
-	return vehicles->get(entity_id);
-}
-
-template<>
-inline IActor* GetEntity<IActor>(int entity_id)
-{
-	IActorsComponent* actors = OmpManager::Get()->GetActors();
-	if (actors == nullptr)
-	{
-		return nullptr;
-	}
-
-	return actors->get(entity_id);
-}
-
-template<>
-inline IObject* GetEntity<IObject>(int entity_id)
-{
-	IObjectsComponent* objects = OmpManager::Get()->GetObjects();
-	if (objects == nullptr)
-	{
-		return nullptr;
-	}
-
-	return objects->get(entity_id);
+	nlohmann::json ret_value;
+	ret_value["ret_value"] = value;
+	return ret_value;
 }
 
 /// Get player using provided player id, return fail_ret if not available
-#define GET_PLAYER_CHECKED(player_output, playerid, fail_ret) \
-	IPlayer* player_output = GetPlayer(playerid); \
-	if(player_output == nullptr) return fail_ret;
-
-/// Get player object, return fail_ret if not available
-#define GET_PLAYER_OBJECT_CHECKED(object_output, playerid, objectid, fail_ret) \
-	IPlayerObject* object_output = GetPlayerEntity<IPlayerObjectData, IPlayerObject>(playerid, objectid); \
-	if(object_output == nullptr) return fail_ret;
-
-/// Get player textlabels, return fail_ret if not available
-#define GET_PLAYER_TEXT_LABEL_CHECKED(text_label_output, playerid, textlabelid, fail_ret) \
-	IPlayerTextLabel* text_label_output = GetPlayerEntity<IPlayerTextLabelData, IPlayerTextLabel>(playerid, textlabelid); \
-	if(text_label_output == nullptr) return fail_ret;
-
-#define GET_ENTITY_CHECKED(entity_output, entity_type, entity_id, fail_ret) \
-	entity_type* entity_output = GetEntity<entity_type>(entity_id); \
-	if(entity_output == nullptr) return fail_ret;
+#define GET_POOL_ENTITY_CHECKED(pool_instance, entity_type, entity_mem_addr, entity_output) \
+	if(pool_instance == nullptr) \
+		return "{\"ret_value\":{\"error\":\"Pool for " STRINGIFY(#entity_type) " is unavailable.\"}}"_json; \
+	entity_type* entity_output = reinterpret_cast<entity_type*>(uintptr_t(entity_mem_addr)); \
+	if(entity_output == nullptr) \
+		return "{\"ret_value\":{\"error\":\"Invalid " STRINGIFY(#entity_type) " entity pointer " STRINGIFY(#entity_mem_addr) ".\"}}"_json;
