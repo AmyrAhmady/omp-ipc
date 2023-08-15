16a909cefbb573e3caa09c8cb280bb94e638e42a
rave <62031908+ravesamp@users.noreply.github.com>
2023-08-15 18:29:43 +0300

    Update APIs.cpp

--- src/api/scripting/Players/APIs.cpp
+++ src/api/scripting/Players/APIs.cpp
@@ -8,7 +8,12 @@ IPC_API(Player_SendMessage, const nlohmann::json& params)
 	return NO_DATA_SUCCESS_RETURN;
 }
 
-IPC_API(Player_SetCameraPos, const nlohmann::json& params)
+IPC_API(Core_SendMessageToAll, const nlohmann::json& params)
+{
+    // TODO
+}
+
+IPC_API(Player_SetCameraPosition, const nlohmann::json& params)
 {
 	GET_POOL_ENTITY_CHECKED(OmpManager::Get()->players, IPlayer, params["player"], player);
 	player->setCameraPosition({ params["x"], params["y"], params["z"] });
@@ -41,6 +46,11 @@ IPC_API(Player_SetWeather, const nlohmann::json& params)
 	GET_POOL_ENTITY_CHECKED(OmpManager::Get()->players, IPlayer, params["player"], player);
 	player->setWeather(params["weatherid"]);
 	return NO_DATA_SUCCESS_RETURN;
+}
+
+IPC_API(Player_GetWeather, const nlohmann::json& params)
+{
+    // TODO
 }
 
 IPC_API(Player_SetSkin, const nlohmann::json& params)
@@ -85,6 +95,11 @@ IPC_API(Player_CreateExplosion, const nlohmann::json& params)
 	return NO_DATA_SUCCESS_RETURN;
 }
 
+IPC_API(Core_CreateExplosion, const nlohmann::json& params)
+{
+    // TODO
+}
+
 IPC_API(Player_PlayAudioStream, const nlohmann::json& params)
 {
 	GET_POOL_ENTITY_CHECKED(OmpManager::Get()->players, IPlayer, params["player"], player);
@@ -99,11 +114,22 @@ IPC_API(Player_StopAudioStream, const nlohmann::json& params)
 	return NO_DATA_SUCCESS_RETURN;
 }
 
+IPC_API(Player_SendDeathMessage, const nlohmann::json& params)
+{
+    // TODO
+}
+
 IPC_API(Player_ToggleWidescreen, const nlohmann::json& params)
 {
 	GET_POOL_ENTITY_CHECKED(OmpManager::Get()->players, IPlayer, params["player"], player);
 	player->useWidescreen(params["enable"]);
 	return NO_DATA_SUCCESS_RETURN;
+}
+
+IPC_API(Player_IsWidescreenToggled, const nlohmann::json& params)
+{
+	GET_POOL_ENTITY_CHECKED(OmpManager::Get()->players, IPlayer, params["player"], player);
+	return RETURN_VALUE(player->hasWidescreen());
 }
 
 IPC_API(Player_SetHealth, const nlohmann::json& params)
@@ -113,11 +139,23 @@ IPC_API(Player_SetHealth, const nlohmann::json& params)
 	return NO_DATA_SUCCESS_RETURN;
 }
 
+IPC_API(Player_GetHealth, const nlohmann::json& params)
+{
+	GET_POOL_ENTITY_CHECKED(OmpManager::Get()->players, IPlayer, params["player"], player);
+	return RETURN_VALUE(player->getHealth());
+}
+
 IPC_API(Player_SetArmour, const nlohmann::json& params)
 {
 	GET_POOL_ENTITY_CHECKED(OmpManager::Get()->players, IPlayer, params["player"], player);
 	player->setArmour(params["armour"]);
 	return NO_DATA_SUCCESS_RETURN;
+}
+
+IPC_API(Player_GetArmour, const nlohmann::json& params)
+{
+	GET_POOL_ENTITY_CHECKED(OmpManager::Get()->players, IPlayer, params["player"], player);
+	return RETURN_VALUE(player->getArmour());
 }
 
 IPC_API(Player_SetTeam, const nlohmann::json& params)
@@ -127,11 +165,57 @@ IPC_API(Player_SetTeam, const nlohmann::json& params)
 	return NO_DATA_SUCCESS_RETURN;
 }
 
+IPC_API(Player_GetTeam, const nlohmann::json& params)
+{
+    // TODO
+}
+
 IPC_API(Player_SetScore, const nlohmann::json& params)
 {
 	GET_POOL_ENTITY_CHECKED(OmpManager::Get()->players, IPlayer, params["player"], player);
 	player->setScore(params["score"]);
 	return NO_DATA_SUCCESS_RETURN;
+}
+
+IPC_API(Player_GetScore, const nlohmann::json& params)
+{
+	GET_POOL_ENTITY_CHECKED(OmpManager::Get()->players, IPlayer, params["player"], player);
+	return RETURN_VALUE(player->getScore());
+}
+
+IPC_API(Player_GetSkin, const nlohmann::json& params)
+{
+	GET_POOL_ENTITY_CHECKED(OmpManager::Get()->players, IPlayer, params["player"], player);
+	return RETURN_VALUE(player->getSkin());
+}
+
+IPC_API(Player_SetColour, const nlohmann::json& params)
+{
+    GET_POOL_ENTITY_CHECKED(OmpManager::Get()->players, IPlayer, params["player"], player);
+    player->setColour(Colour::FromRGBA(params["colour"]));
+    return NO_DATA_SUCCESS_RETURN;
+}
+
+IPC_API(Player_GetColour, const nlohmann::json& params)
+{
+    GET_POOL_ENTITY_CHECKED(OmpManager::Get()->players, IPlayer, params["player"], player);
+    return RETURN_VALUE(player->getColour().RGBA());
+}
+
+IPC_API(Player_GetDefaultColour, const nlohmann::json& params)
+{
+    // TODO
+}
+
+IPC_API(Player_GetDrunkLevel, const nlohmann::json& params)
+{
+	GET_POOL_ENTITY_CHECKED(OmpManager::Get()->players, IPlayer, params["player"], player);
+	return RETURN_VALUE(player->getDrunkLevel());
+}
+
+IPC_API(Player_GiveWeapon, const nlohmann::json& params)
+{
+    // TODO
 }
 
 IPC_API(Player_RemoveWeapon, const nlohmann::json& params)
@@ -141,11 +225,55 @@ IPC_API(Player_RemoveWeapon, const nlohmann::json& params)
 	return NO_DATA_SUCCESS_RETURN;
 }
 
+IPC_API(Player_GetMoney, const nlohmann::json& params)
+{
+	GET_POOL_ENTITY_CHECKED(OmpManager::Get()->players, IPlayer, params["player"], player);
+	return RETURN_VALUE(player->getMoney());
+}
+
 IPC_API(Player_ResetMoney, const nlohmann::json& params)
 {
 	GET_POOL_ENTITY_CHECKED(OmpManager::Get()->players, IPlayer, params["player"], player);
 	player->resetMoney();
 	return NO_DATA_SUCCESS_RETURN;
+}
+
+IPC_API(Player_SetName, const nlohmann::json& params)
+{
+    // TODO
+}
+
+IPC_API(Player_GetName, const nlohmann::json& params)
+{
+    // TODO
+}
+
+IPC_API(Player_GetState, const nlohmann::json& params)
+{
+	GET_POOL_ENTITY_CHECKED(OmpManager::Get()->players, IPlayer, params["player"], player);
+	return RETURN_VALUE(player->getState());
+}
+
+IPC_API(Player_GetPing, const nlohmann::json& params)
+{
+    // TODO
+}
+
+IPC_API(Player_GetWeapon, const nlohmann::json& params)
+{
+    // TODO
+}
+
+IPC_API(Player_SetTime, const nlohmann::json& params)
+{
+    GET_POOL_ENTITY_CHECKED(OmpManager::Get()->players, IPlayer, params["player"], player);
+	player->setTime(std::chrono::hours(params["hour"]), std::chrono::minutes(params["minute"]));
+	return NO_DATA_SUCCESS_RETURN;
+}
+
+IPC_API(Player_GetTime, const nlohmann::json& params)
+{
+    // TODO
 }
 
 IPC_API(Player_ToggleClock, const nlohmann::json& params)
@@ -155,11 +283,35 @@ IPC_API(Player_ToggleClock, const nlohmann::json& params)
 	return NO_DATA_SUCCESS_RETURN;
 }
 
+IPC_API(Player_HasClockEnabled, const nlohmann::json& params)
+{
+	GET_POOL_ENTITY_CHECKED(OmpManager::Get()->players, IPlayer, params["player"], player);
+	return RETURN_VALUE(player->hasClock());
+}
+
 IPC_API(Player_ForceClassSelection, const nlohmann::json& params)
 {
 	GET_POOL_ENTITY_CHECKED(OmpManager::Get()->players, IPlayer, params["player"], player);
 	player->forceClassSelection();
 	return NO_DATA_SUCCESS_RETURN;
+}
+
+IPC_API(Player_GetWantedLevel, const nlohmann::json& params)
+{
+	GET_POOL_ENTITY_CHECKED(OmpManager::Get()->players, IPlayer, params["player"], player);
+	return RETURN_VALUE(player->getWantedLevel());
+}
+
+IPC_API(Player_SetFightingStyle, const nlohmann::json& params)
+{
+    GET_POOL_ENTITY_CHECKED(OmpManager::Get()->players, IPlayer, params["player"], player);
+	player->setFightingStyle(PlayerFightingStyle(params["style"]));
+	return NO_DATA_SUCCESS_RETURN;
+}
+
+IPC_API(Player_GetFightingStyle, const nlohmann::json& params)
+{
+    // TODO
 }
 
 IPC_API(Player_SetVelocity, const nlohmann::json& params)
@@ -169,11 +321,55 @@ IPC_API(Player_SetVelocity, const nlohmann::json& params)
 	return NO_DATA_SUCCESS_RETURN;
 }
 
-IPC_API(Player_SetPos, const nlohmann::json& params)
+IPC_API(Player_GetVelocity, const nlohmann::json& params)
+{
+    // TODO
+}
+
+IPC_API(Player_GetCameraPosition, const nlohmann::json& params)
+{
+    // TODO
+}
+
+IPC_API(Player_GetDistanceFromPoint, const nlohmann::json& params)
+{
+    // TODO
+}
+
+IPC_API(Player_GetInterior, const nlohmann::json& params)
+{
+	GET_POOL_ENTITY_CHECKED(OmpManager::Get()->players, IPlayer, params["player"], player);
+	return RETURN_VALUE(player->getInterior());
+}
+
+IPC_API(Player_SetPosition, const nlohmann::json& params)
 {
 	GET_POOL_ENTITY_CHECKED(OmpManager::Get()->players, IPlayer, params["player"], player);
 	player->setPosition({ params["x"], params["y"], params["z"] });
 	return NO_DATA_SUCCESS_RETURN;
+}
+
+IPC_API(Player_GetPosition, const nlohmann::json& params)
+{
+    // TODO
+}
+
+IPC_API(Player_GetVirtualWorld, const nlohmann::json& params)
+{
+	GET_POOL_ENTITY_CHECKED(OmpManager::Get()->players, IPlayer, params["player"], player);
+	return RETURN_VALUE(player->getVirtualWorld());
+}
+
+IPC_API(Player_IsNPC, const nlohmann::json& params)
+{
+    // TODO
+}
+
+IPC_API(Player_IsStreamedIn, const nlohmann::json& params)
+{
+	GET_POOL_ENTITY_CHECKED(OmpManager::Get()->players, IPlayer, params["player"], player);
+    GET_POOL_ENTITY_CHECKED(OmpManager::Get()->players, IPlayer, params["other"], other);
+	return RETURN_VALUE(player->isStreamedInForPlayer(*other));
 }
 
 IPC_API(Player_PlaySound, const nlohmann::json& params)
@@ -183,11 +379,79 @@ IPC_API(Player_PlaySound, const nlohmann::json& params)
 	return NO_DATA_SUCCESS_RETURN;
 }
 
+IPC_API(Player_SpectatePlayer, const nlohmann::json& params)
+{
+    GET_POOL_ENTITY_CHECKED(OmpManager::Get()->players, IPlayer, params["player"], player);
+    GET_POOL_ENTITY_CHECKED(OmpManager::Get()->players, IPlayer, params["target"], target);
+    player->spectatePlayer(*target, PlayerSpectateMode(params["mode"]));
+    return NO_DATA_SUCCESS_RETURN;
+}
+
+IPC_API(Player_SpectateVehicle, const nlohmann::json& params)
+{
+    GET_POOL_ENTITY_CHECKED(OmpManager::Get()->players, IPlayer, params["player"], player);
+    GET_POOL_ENTITY_CHECKED(OmpManager::Get()->vehicles, IVehicle, params["target"], target);
+    player->spectateVehicle(*target, PlayerSpectateMode(params["mode"]));
+    return NO_DATA_SUCCESS_RETURN;
+}
+
 IPC_API(Player_SetVirtualWorld, const nlohmann::json& params)
 {
 	GET_POOL_ENTITY_CHECKED(OmpManager::Get()->players, IPlayer, params["player"], player);
 	player->setVirtualWorld(params["vw"]);
 	return NO_DATA_SUCCESS_RETURN;
+}
+
+IPC_API(Player_SetWorldBounds, const nlohmann::json& params)
+{
+    // TODO
+}
+
+IPC_API(Player_GetWorldBounds, const nlohmann::json& params)
+{
+    // TODO
+}
+
+IPC_API(Player_ClearAnimations, const nlohmann::json& params)
+{
+    // TODO: This must be fixed on client side
+	// At the moment ClearAnimations flushes all tasks applied to player
+	// Including driving, siting in vehicle, shooting, jumping, or any sort of a task
+	// And it doesn't just clear applied animations, in order to keep it compatible with
+	// Current samp scripts without requiring a change, we call IPlayer::clearTasks temporarily.
+    GET_POOL_ENTITY_CHECKED(OmpManager::Get()->players, IPlayer, params["player"], player);
+    player->clearTasks(PlayerAnimationSyncType(params["syncType"]));
+    return NO_DATA_SUCCESS_RETURN;
+}
+
+IPC_API(Player_GetLastShotVectors, const nlohmann::json& params)
+{
+    // TODO
+}
+
+IPC_API(GetPlayerCameraTargetPlayer, const nlohmann::json& params)
+{
+    // TODO
+}
+
+IPC_API(GetPlayerCameraTargetActor, const nlohmann::json& params)
+{
+    // TODO
+}
+
+IPC_API(GetPlayerCameraTargetObject, const nlohmann::json& params)
+{
+    // TODO
+}
+
+IPC_API(GetPlayerCameraTargetVehicle, const nlohmann::json& params)
+{
+    // TODO
+}
+
+IPC_API(IsPlayerConnected, const nlohmann::json& params)
+{
+    // TODO
 }
 
 IPC_API(Player_PutInVehicle, const nlohmann::json& params)
@@ -205,11 +469,27 @@ IPC_API(Player_RemoveBuilding, const nlohmann::json& params)
 	return NO_DATA_SUCCESS_RETURN;
 }
 
+IPC_API(Player_GetBuildingsRemoved, const nlohmann::json& params)
+{
+	GET_POOL_ENTITY_CHECKED(OmpManager::Get()->players, IPlayer, params["player"], player);
+	return RETURN_VALUE(player->getDefaultObjectsRemoved());
+}
+
+IPC_API(RemovePlayerFromVehicle, const nlohmann::json& params)
+{
+    // TODO
+}
+
 IPC_API(Player_RemoveMapIcon, const nlohmann::json& params)
 {
 	GET_POOL_ENTITY_CHECKED(OmpManager::Get()->players, IPlayer, params["player"], player);
 	player->unsetMapIcon(params["iconID"]);
 	return NO_DATA_SUCCESS_RETURN;
+}
+
+IPC_API(SetPlayerMapIcon, const nlohmann::json& params)
+{
+    // TODO
 }
 
 IPC_API(Player_ResetWeapons, const nlohmann::json& params)
@@ -219,6 +499,11 @@ IPC_API(Player_ResetWeapons, const nlohmann::json& params)
 	return NO_DATA_SUCCESS_RETURN;
 }
 
+IPC_API(SetPlayerAmmo, const nlohmann::json& params)
+{
+    // TODO
+}
+
 IPC_API(Player_SetArmedWeapon, const nlohmann::json& params)
 {
 	GET_POOL_ENTITY_CHECKED(OmpManager::Get()->players, IPlayer, params["player"], player);
@@ -226,10 +511,31 @@ IPC_API(Player_SetArmedWeapon, const nlohmann::json& params)
 	return NO_DATA_SUCCESS_RETURN;
 }
 
-IPC_API(Player_SetPosFindZ, const nlohmann::json& params)
+IPC_API(Player_SetChatBubble, const nlohmann::json& params)
+{
+    GET_POOL_ENTITY_CHECKED(OmpManager::Get()->players, IPlayer, params["player"], player);
+	player->setChatBubble(params["text"], Colour::FromRGBA(params["colour"]), params["drawdistance"], std::chrono::milliseconds(params["expiretime"]));
+	return NO_DATA_SUCCESS_RETURN;
+}
+
+IPC_API(Player_SetPositionFindZ, const nlohmann::json& params)
 {
 	GET_POOL_ENTITY_CHECKED(OmpManager::Get()->players, IPlayer, params["player"], player);
 	player->setPositionFindZ({ params["x"], params["y"], params["z"] });
+	return NO_DATA_SUCCESS_RETURN;
+}
+
+IPC_API(Player_SetSkillLevel, const nlohmann::json& params)
+{
+    GET_POOL_ENTITY_CHECKED(OmpManager::Get()->players, IPlayer, params["player"], player);
+	player->setSkillLevel(PlayerWeaponSkill(params["weapon"]), params["level"]);
+	return NO_DATA_SUCCESS_RETURN;
+}
+
+IPC_API(Player_SetSpecialAction, const nlohmann::json& params)
+{
+    GET_POOL_ENTITY_CHECKED(OmpManager::Get()->players, IPlayer, params["player"], player);
+	player->setAction(PlayerSpecialAction(params["action"]));
 	return NO_DATA_SUCCESS_RETURN;
 }
 
@@ -255,6 +561,21 @@ IPC_API(Player_ToggleSpectating, const nlohmann::json& params)
 	return NO_DATA_SUCCESS_RETURN;
 }
 
+IPC_API(ApplyAnimation, const nlohmann::json& params)
+{
+    // TODO
+}
+
+IPC_API(GetAnimationName, const nlohmann::json& params)
+{
+    // TODO
+}
+
+IPC_API(EditAttachedObject, const nlohmann::json& params)
+{
+    // TODO
+}
+
 IPC_API(Player_EnableCameraTarget, const nlohmann::json& params)
 {
 	GET_POOL_ENTITY_CHECKED(OmpManager::Get()->players, IPlayer, params["player"], player);
@@ -269,12 +590,127 @@ IPC_API(Player_EnableStuntBonus, const nlohmann::json& params)
 	return NO_DATA_SUCCESS_RETURN;
 }
 
+IPC_API(EnableStuntBonusForAll, const nlohmann::json& params)
+{
+    // TODO
+}
+
+IPC_API(Player_GetAmmo, const nlohmann::json& params)
+{
+	GET_POOL_ENTITY_CHECKED(OmpManager::Get()->players, IPlayer, params["player"], player);
+	return RETURN_VALUE(player->getArmedWeaponAmmo());
+}
+
+IPC_API(Player_GetAnimationIndex, const nlohmann::json& params)
+{
+	GET_POOL_ENTITY_CHECKED(OmpManager::Get()->players, IPlayer, params["player"], player);
+	return RETURN_VALUE(player->getAnimationData().ID);
+}
+
+IPC_API(GetPlayerFacingAngle, const nlohmann::json& params)
+{
+    // TODO
+}
+
+IPC_API(GetPlayerIp, const nlohmann::json& params)
+{
+    // TODO
+}
+
+IPC_API(Player_GetSpecialAction, const nlohmann::json& params)
+{
+	GET_POOL_ENTITY_CHECKED(OmpManager::Get()->players, IPlayer, params["player"], player);
+	return RETURN_VALUE(player->getAction());
+}
+
+IPC_API(GetPlayerVehicleID, const nlohmann::json& params)
+{
+    // TODO
+}
+
+IPC_API(GetPlayerVehicleSeat, const nlohmann::json& params)
+{
+    // TODO
+}
+
+IPC_API(GetPlayerWeaponData, const nlohmann::json& params)
+{
+    // TODO
+}
+
+IPC_API(Player_GetWeaponState, const nlohmann::json& params)
+{
+	GET_POOL_ENTITY_CHECKED(OmpManager::Get()->players, IPlayer, params["player"], player);
+	return RETURN_VALUE(player->getAimData().weaponState);
+}
+
+IPC_API(InterpolateCameraPos, const nlohmann::json& params)
+{
+    // TODO
+}
+
+IPC_API(InterpolateCameraLookAt, const nlohmann::json& params)
+{
+    // TODO
+}
+
+IPC_API(IsPlayerAttachedObjectSlotUsed, const nlohmann::json& params)
+{
+    // TODO
+}
+
 IPC_API(Player_AttachCameraToObject, const nlohmann::json& params)
 {
 	GET_POOL_ENTITY_CHECKED(OmpManager::Get()->players, IPlayer, params["player"], player);
 	GET_POOL_ENTITY_CHECKED(OmpManager::Get()->objects, IObject, params["object"], object);
 	player->attachCameraToObject(*object);
 	return NO_DATA_SUCCESS_RETURN;
+}
+
+IPC_API(AttachCameraToPlayerObject, const nlohmann::json& params)
+{
+    // TODO
+}
+
+IPC_API(Player_GetCameraAspectRatio, const nlohmann::json& params)
+{
+	GET_POOL_ENTITY_CHECKED(OmpManager::Get()->players, IPlayer, params["player"], player);
+	return RETURN_VALUE(player->getAimData().aspectRatio);
+}
+
+IPC_API(GetPlayerCameraFrontVector, const nlohmann::json& params)
+{
+    // TODO
+}
+
+IPC_API(GetPlayerCameraMode, const nlohmann::json& params)
+{
+    // TODO
+}
+
+IPC_API(GetPlayerKeys, const nlohmann::json& params)
+{
+    // TODO
+}
+
+IPC_API(GetPlayerSurfingVehicleID, const nlohmann::json& params)
+{
+    // TODO
+}
+
+IPC_API(GetPlayerSurfingObjectID, const nlohmann::json& params)
+{
+    // TODO
+}
+
+IPC_API(GetPlayerTargetPlayer, const nlohmann::json& params)
+{
+    // TODO
+}
+
+IPC_API(GetPlayerTargetActor, const nlohmann::json& params)
+{
+    // TODO
 }
 
 IPC_API(Player_DisableRemoteVehicleCollisions, const nlohmann::json& params)
