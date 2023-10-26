9e639de605b3c216f56f22a4fc76819883fa2458
adib-yg <microsoftuser3030@gmail.com>
2023-10-26 21:06:22 +0330

    Update Pickups\Events.hpp

--- src/api/scripting/Pickups/Events.hpp
+++ src/api/scripting/Pickups/Events.hpp
@@ -12,4 +12,43 @@ struct PickupEvents : public PickupEventHandler, public Singleton<PickupEvents>
 		args["pickup"] = uintptr_t(&pickup);
 		MessageSocket::ProcessEvent("playerPickUpPickup", args);
 	}
+	void onPickupStreamIn(IPickup& pickup, IPlayer& player) override
+	{
+		nlohmann::json args;
+		args["player"] = uintptr_t(&player);
+		args["pickup"] = uintptr_t(&pickup);
+		MessageSocket::ProcessEvent("pickupStreamIn", args);
+	}
+	void onPickupStreamOut(IPickup& pickup, IPlayer& player) override
+	{
+		nlohmann::json args;
+		args["player"] = uintptr_t(&player);
+		args["pickup"] = uintptr_t(&pickup);
+		MessageSocket::ProcessEvent("pickupStreamOut", args);
+	}
+
+	/*
+		Per-Player Pickups
+	*/
+	void onPlayerPickUpPlayerPickup(IPlayer& player, IPickup& pickup) override
+	{
+		nlohmann::json args;
+		args["player"] = uintptr_t(&player);
+		args["pickup"] = uintptr_t(&pickup);
+		MessageSocket::ProcessEvent("playerPickUpPlayerPickup", args);
+	}
+	void onPlayerPickupStreamIn(IPickup& pickup, IPlayer& player) override
+	{
+		nlohmann::json args;
+		args["player"] = uintptr_t(&player);
+		args["pickup"] = uintptr_t(&pickup);
+		MessageSocket::ProcessEvent("playerPickupStreamIn", args);
+	}
+	void onPlayerPickupStreamOut(IPickup& pickup, IPlayer& player) override
+	{
+		nlohmann::json args;
+		args["player"] = uintptr_t(&player);
+		args["pickup"] = uintptr_t(&pickup);
+		MessageSocket::ProcessEvent("playerPickupStreamOut", args);
+	}
 };
