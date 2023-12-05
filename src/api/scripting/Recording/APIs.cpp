6060a0e0d03222fe32278f625b5f60dd32e8a1ef
adib-yg <microsoftuser3030@gmail.com>
2023-12-05 03:38:47 +0330

    Recording APIs

---
+++ src/api/scripting/Recording/APIs.cpp
@@ -0,0 +1,26 @@
+#include "../../../message/Handler.hpp"
+#include "../Manager.hpp"
+
+IPC_API(Recording_Start, uintptr_t player, int type, ConstStringRef file)
+{
+	GET_POOL_ENTITY_CHECKED(OmpManager::Get()->players, IPlayer, player, player_);
+	IPlayerRecordingData* recording = queryExtension<IPlayerRecordingData>(player_);
+	if (recording)
+	{
+		recording->start(PlayerRecordingType(type), file);
+		IPC_RETURN();
+	}
+	IPC_RETURN();
+}
+
+IPC_API(Recording_Stop, uintptr_t player)
+{
+	GET_POOL_ENTITY_CHECKED(OmpManager::Get()->players, IPlayer, player, player_);
+	IPlayerRecordingData* recording = queryExtension<IPlayerRecordingData>(player_);
+	if (recording)
+	{
+		recording->stop();
+		IPC_RETURN();
+	}
+	IPC_RETURN();
+}
