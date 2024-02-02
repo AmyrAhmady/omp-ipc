5525df1205fae3ab72e235fa13012b70bbf2d98a
iAmir <hhm6@yahoo.com>
2024-02-02 09:03:11 +0330

    create api docs generator script

---
+++ generate-docs.js
@@ -0,0 +1,78 @@
+const fs = require("fs");
+const path = require("path");
+
+const getLineBreakType = (content) => {
+  const indexOfLF = content.indexOf("\n", 1);
+
+  if (indexOfLF === -1) {
+    if (content.indexOf("\r") !== -1) return "\r";
+    return "\n";
+  }
+
+  if (content[indexOfLF - 1] === "\r") return "\r\n";
+};
+
+const getParamTypeForDocs = (type) => {
+  switch (type) {
+    case "int":
+      return "int";
+    case "ConstStringRef":
+      return "string";
+    case "bool":
+      return "bool";
+    case "float":
+      return "float";
+    case "uintptr_t":
+      return "pointer";
+    case "uint8_t":
+      return "uint8";
+    case "uint32_t":
+      return "uint32";
+    default:
+      throw `Error converting type: can not convert ${type}`;
+  }
+};
+
+const files = fs.readdirSync("./src/api/scripting", { recursive: true });
+files.forEach((file) => {
+  if (file.includes("APIs.cpp")) {
+    const filePath = path.join("./src/api/scripting", file);
+    const apiFilePath = path.join("./apidocs", `${path.dirname(file)}.json`);
+    const api = fs.existsSync(apiFilePath)
+      ? JSON.parse(fs.readFileSync(apiFilePath).toString())
+      : {};
+
+    api.functions = [];
+
+    const content = fs.readFileSync(filePath).toString();
+
+    const regex =
+      getLineBreakType(content) === "\n"
+        ? /IPC_API\((.*?)\)\n\{\n([\S\s]*?)\n\}\n\n/gm
+        : /IPC_API\((.*?)\)\r\n\{\r\n([\S\s]*?)\r\n\}\r\n\r\n/gm;
+
+    let m;
+    while ((m = regex.exec(content)) !== null) {
+      if (m.index === regex.lastIndex) {
+        regex.lastIndex++;
+      }
+
+      if (m.length === 3) {
+        const apiDefParams = m[1].split(", ");
+        const funcName = apiDefParams[0];
+        apiDefParams.shift();
+        const params = apiDefParams.map((paramWithType) => {
+          const p = paramWithType.split(" ");
+          return { name: p[1], type: getParamTypeForDocs(p[0]) };
+        });
+
+        api.functions.push({
+          name: funcName,
+          params: params,
+        });
+      }
+    }
+
+    fs.writeFileSync(apiFilePath, JSON.stringify(api, undefined, 2));
+  }
+});
